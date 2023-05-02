7bc732f1499b7752e373a16597aba7eba33d9ecd
Kagetsu Chiwa <132134394+2021212205197@users.noreply.github.com>
2023-05-02 13:20:42 +0800

    Create index.js

---
+++ Web/Project1/js/index.js
@@ -0,0 +1,48 @@
+var ale1 = document.getElementById("alert1");
+ale1.onclick=function () {
+    alert("test");
+}
+var ale2 = document.getElementById("alert2");
+ale2.onclick=function () {
+    alert("test");
+}
+var ale3 = document.getElementById("alert3");
+ale3.onclick=function () {
+    alert("test");
+}
+var ale4 = document.getElementById("alert4");
+ale4.onclick=function () {
+    alert("test");
+}
+var ale5 = document.getElementById("alert5");
+ale5.onclick=function () {
+    alert("test");
+}
+var ale6 = document.getElementById("alert6");
+ale6.onclick=function () {
+    alert("test");
+}
+var ale7 = document.getElementById("alert7");
+ale6.onclick=function () {
+    alert("test");
+}
+var ale8 = document.getElementById("alert8");
+ale6.onclick=function () {
+    alert("test");
+}
+var ale9 = document.getElementById("alert9");
+ale6.onclick=function () {
+    alert("test");
+}
+var ale10 = document.getElementById("alert10");
+ale6.onclick=function () {
+    alert("test");
+}
+var ale11 = document.getElementById("alert11");
+ale6.onclick=function () {
+    alert("test");
+}
+var ale12 = document.getElementById("alert12");
+ale6.onclick=function () {
+    alert("test");
+}
